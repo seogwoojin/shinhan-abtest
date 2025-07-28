4dd9090ca81e54b2f58a1dc99e33f332bc4ec60a
seogwoojin <server@uos.ac.kr>
2025-07-28 11:09:17 +0900

    feat: init project

---
+++ src/main/kotlin/com/example/finsync/FinsyncApplication.kt
@@ -0,0 +1,11 @@
+package com.example.finsync
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinsyncApplication
+
+fun main(args: Array<String>) {
+    runApplication<FinsyncApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/finsync/FinsyncApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.finsync
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinsyncApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
